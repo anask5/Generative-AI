52b86d4afe59602a32747f09d210f5932e9886e7
Anas Khan <leobatesinc@gmail.com>
2025-12-01 13:00:58 +0530

    Add conditional statements for marriage eligibility

---
+++ conditonal.py
@@ -0,0 +1,12 @@
+
+#conditonal statements 
+#if - this statement execute when condition is true 
+#elif - this statement execute when condition is true 
+#else - this statement executes if and elif condition is false 
+age = int(input("enter your age"))
+if age>60:
+   print("you are too old for marriage")
+elif age<18:
+  print("You are too young for marriage")
+else:
+  print("we will find a perfect match for you")
