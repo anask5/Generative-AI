d5db45c2053b4b51baa5e2c5923ac12872554c3f
Anas Khan <leobatesinc@gmail.com>
2025-12-01 13:05:53 +0530

    Add fruit input collection to appendlist.py

---
+++ appendlist.py
@@ -0,0 +1,17 @@
+
+
+fruit = []
+f1 = input("Enter Fruits Name")
+fruit.append(f1)
+f2 = input("Enter Fruits Name")
+fruit.append(f2)
+f3 = input("Enter Fruits Name")
+fruit.append(f3)
+f4 = input("Enter Fruits Name")
+fruit.append(f4)
+f5 = input("Enter Fruits Name")
+fruit.append(f5)
+f6 = input("Enter Fruits Name")
+fruit.append(f6)
+
+print(fruit)
