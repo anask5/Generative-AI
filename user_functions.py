e650f767af0de36296e78026c734fc29d5fce32b
Anas Khan <leobatesinc@gmail.com>
2025-12-01 13:18:40 +0530

    Create add function for user-defined addition
    Define a user function to add two numbers.

---
+++ user_functions.py
@@ -0,0 +1,6 @@
+#user defined function -- defined by user according to the need
+def add():
+  a = int(input("Enter first number"))
+  b = int(input("Enter second number"))
+  return a+b
+  print(add())
